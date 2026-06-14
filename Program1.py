4de7ea495bba823e937c955b723515249b5c8e17
Sanika <105974093+Sanika163@users.noreply.github.com>
2026-06-15 00:20:26 +0530

    Add files via upload

---
+++ Program1.py
@@ -0,0 +1,7 @@
+#Program to display: Value, Type, Memory Address
+
+x = 11
+
+print(x)
+print(type(x))
+print(id(x))
---
+++ Program2.py
@@ -0,0 +1,14 @@
+#Write program that accepts two numbers from user and print their: Addition, Subtraction, Multiplication, Division
+
+print("Enter First Number: ")
+No1 = int(input())
+
+print("Enter second Number: ")
+No2 = int(input())
+
+print("Addition: ", No1+No2)
+print("Subtraction: ", No1-No2)
+print("Multiplication: ", No1*No2)
+print("Division: ", No1/No2)
+
+
